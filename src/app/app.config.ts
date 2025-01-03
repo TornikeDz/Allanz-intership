f1f8c694c704d314018cdc88668a9ed40e4994bb
eid iepoh <tornike.dzmanashvili@gmail.com>
2025-01-03 15:16:22 +0400

    created routing module and configured routing for all pages. created home pages as a default page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,7 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [importProvidersFrom(AppRoutingModule), provideClientHydration()]
 };
