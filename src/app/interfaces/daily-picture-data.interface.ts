00bf911748d9d24d4028d980a50670c442c683d9
eid iepoh <tornike.dzmanashvili@gmail.com>
2025-01-06 15:13:25 +0400

    added http service and endpoints for daily image service

---
+++ src/app/interfaces/daily-picture-data.interface.ts
@@ -0,0 +1,9 @@
+export interface DailyPictureData {
+    date: string;
+    explanation: string;
+    hdurl: string;
+    media_type: 'image' | 'video';
+    service_version: string;
+    title: string;
+    url: string;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, OnInit } from '@angular/core';
+import { DailyPictureService } from '../../services/daily-picture.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { catchError, of } from 'rxjs';
+import { DailyPictureData } from '../../interfaces/daily-picture-data.interface';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +12,40 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  public dailyTitle: string = "";
+  public dailyImage: string = "";
+  public dailyDescription: string = "";
 
+  constructor(
+    private readonly dailyPictureService: DailyPictureService, 
+    private readonly destroyRef: DestroyRef,
+    private readonly cdr: ChangeDetectorRef
+  ){}
+
+  ngOnInit(): void {
+    this.dailyPictureData();
+  }
+
+  public dailyPictureData() {
+    this.dailyPictureService.getDailyPicture().pipe(
+      catchError(error => {
+        console.error('Something went wrong with retrieving daily picture data', error);
+        return of(null);
+      }),
+      takeUntilDestroyed(this.destroyRef)
+    )
+    .subscribe((dailyPictureData) => {
+      if(dailyPictureData) {
+        this.handleDailyPictureData(dailyPictureData);
+      }
+    })
+  }
+
+  public handleDailyPictureData(data: DailyPictureData) {
+    this.dailyTitle = data.title;
+    this.dailyImage = data.url;
+    this.dailyDescription = data.explanation;
+    this.cdr.markForCheck()
+  }
 }
