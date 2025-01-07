a595ce09e09d942f35c0be02be5d2973eb5f7d7b
eid iepoh <tornike.dzmanashvili@gmail.com>
2025-01-08 01:40:10 +0400

    installed and configured swiperJS library and created components for different kind of cards

---
+++ src/app/pages/swiper/components/planetary-card/planetary-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planetary-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './planetary-card.component.html',
+  styleUrl: './planetary-card.component.scss'
+})
+export class PlanetaryCardComponent {
+
+}
---
+++ src/app/pages/swiper/components/text-card/text-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-card.component.html',
+  styleUrl: './text-card.component.scss'
+})
+export class TextCardComponent {
+
+}
